911f37824c331d8565dfffc750ded9c7be89b4be
Selvano Sena Aji Patria <vanoaji402@gmail.com>
2026-04-08 00:21:19 +0800

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -44,7 +44,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
-
+          
   void _incrementCounter() {
     setState(() {
       _counter++;
@@ -53,7 +53,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
     // by the _incrementCounter method above.
     //
     // The Flutter framework has been optimized to make rerunning build methods
