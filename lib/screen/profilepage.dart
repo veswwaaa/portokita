d4545ca74760fa8e5476d20b98bcfcf99fed743b
Selvano Sena Aji Patria <vanoaji402@gmail.com>
2026-05-17 12:29:07 +0800

    updated

--- lib/screen/profilepage.dart
+++ lib/screen/profilepage.dart
@@ -349,7 +349,7 @@ class _ProfilePageState extends State<ProfilePage> {
 
                   const SizedBox(height: 20),
 
-                  // ── Bagian Portofolio & Menu
+                  //Bagian Portofolio & Menu
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
